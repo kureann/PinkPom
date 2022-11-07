6ded043007a7a9ac10d65f250c859cc6b0713a2f
Claire <113581844+kureann@users.noreply.github.com>
2022-11-07 21:52:41 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+var minutes = 25;
+var seconds = "00";
+
+var click = new Audio("Click.mp3");
+var bell = new Audio("Bell.mp3");
+
+function template() {
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}
+
+function start () {
+    click.play()
+
+    minutes = 24;
+    seconds = 59;
+
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    var minutes_interval = setInterval(minutesTimer, 60000);
+    var seconds_interval = setInterval(secondsTimer, 1000);
+
+    function minutesTimer() {
+        minutes = minutes - 1;
+        document.getElementById("minutes").innerHTML = minutes;
+    }
+
+    function secondsTimer() {
+        seconds = seconds - 1;
+        document.getElementById("seconds").innerHTML = seconds;
+
+        if(seconds <= 0) {
+            if(minutes <= 0) {
+                clearInterval(minutes_interval);
+                clearInterval(seconds_interval);
+
+                document.getElementById("done").innerHTML =
+                    "Congrats Blinkeu! You completed the session, take a break!";
+                document.getElementById("done").classList.add("show message");
+
+                bell.play ();
+            }
+            seconds = 60
+        }
+    }
+}
